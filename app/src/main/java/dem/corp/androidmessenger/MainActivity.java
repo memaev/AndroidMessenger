6d843925f398bd5c5ddc1a473c74e06007fbbced
Mike Maevsky <57516381+memaev@users.noreply.github.com>
2023-07-25 16:51:49 +0300

    Initial commit

---
+++ app/src/main/java/dem/corp/androidmessenger/MainActivity.java
@@ -0,0 +1,22 @@
+package dem.corp.androidmessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (FirebaseAuth.getInstance().getCurrentUser()==null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
+}
