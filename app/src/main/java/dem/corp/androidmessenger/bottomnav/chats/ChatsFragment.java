59aab6737d082fec49e2bb04ddc3c4600716c948
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2024-02-09 18:32:35 +0200

    Урок 5. Добавлена загрузка чатов пользователя из базы данных.

--- app/src/main/java/dem/corp/androidmessenger/bottomnav/chats/ChatsFragment.java
+++ app/src/main/java/dem/corp/androidmessenger/bottomnav/chats/ChatsFragment.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 import dem.corp.androidmessenger.chats.Chat;
 import dem.corp.androidmessenger.chats.ChatsAdapter;
@@ -30,12 +40,35 @@ public class ChatsFragment extends Fragment {
     }
 
     private void loadChats(){
-        ArrayList<Chat> chats = new ArrayList<Chat>();
-        chats.add(new Chat("123", "Test chat 1", "123123", "123456"));
-        chats.add(new Chat("124", "Test chat 2", "123123", "123456"));
-        chats.add(new Chat("125", "Test chat 3", "123123", "123456"));
+        ArrayList<Chat> chats = new ArrayList<>();
 
-        binding.chatsRv.setLayoutManager(new LinearLayoutManager(getContext()));
-        binding.chatsRv.setAdapter(new ChatsAdapter(chats));
+        String uid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        FirebaseDatabase.getInstance().getReference().addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String chatsStr = Objects.requireNonNull(snapshot.child("Users").child(uid).child("chats").getValue()).toString();
+                String[] chatsIds = chatsStr.split(",");
+
+                for (String chatId : chatsIds){
+                    DataSnapshot chatSnapshot = snapshot.child("Chats").child(chatId);
+                    String userId1 = Objects.requireNonNull(chatSnapshot.child("user1").getValue()).toString();
+                    String userId2 = Objects.requireNonNull(chatSnapshot.child("user2").getValue()).toString();
+
+                    String chatUserId = (uid.equals(userId1)) ? userId2 : userId1;
+                    String chatName = Objects.requireNonNull(snapshot.child("Users").child(chatUserId).child("username").getValue()).toString();
+
+                    Chat chat = new Chat(chatId, chatName, userId1, userId2);
+                    chats.add(chat);
+                }
+
+                binding.chatsRv.setLayoutManager(new LinearLayoutManager(getContext()));
+                binding.chatsRv.setAdapter(new ChatsAdapter(chats));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Failed to get user chats", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
