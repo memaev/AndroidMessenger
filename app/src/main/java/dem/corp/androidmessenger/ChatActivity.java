75247aa20ea97c8bd1cbdc0d391b09d60f41bf37
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2024-02-22 21:35:13 +0200

    Урок 8 (финальный): добавлен функционал для отправки сообщений

--- app/src/main/java/dem/corp/androidmessenger/ChatActivity.java
+++ app/src/main/java/dem/corp/androidmessenger/ChatActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import dem.corp.androidmessenger.databinding.ActivityChatBinding;
 import dem.corp.androidmessenger.message.Message;
@@ -28,11 +34,38 @@ public class ChatActivity extends AppCompatActivity {
         binding = ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        loadMessages();
+        String chatId = getIntent().getStringExtra("chatId");
+
+        loadMessages(chatId);
+
+        binding.sendMessageBtn.setOnClickListener(v -> {
+            String message = binding.messageEt.getText().toString();
+            if (message.isEmpty()){
+                Toast.makeText(this, "Message field cannot be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+            String date = simpleDateFormat.format(new Date());
+
+            binding.messageEt.setText(""); //clearing the edit text
+            sendMessage(chatId, message, date);
+        });
     }
 
-    private void loadMessages(){
-        String chatId = getIntent().getStringExtra("chatId");
+    private void sendMessage(String chatId, String message, String date){
+        if (chatId==null) return;
+
+        HashMap<String, String> messageInfo = new HashMap<>();
+        messageInfo.put("text", message);
+        messageInfo.put("ownerId", Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+        messageInfo.put("date", date);
+
+        FirebaseDatabase.getInstance().getReference().child("Chats").child(chatId)
+                .child("messages").push().setValue(messageInfo);
+    }
+
+    private void loadMessages(String chatId){
         if (chatId==null) return;
 
         FirebaseDatabase.getInstance().getReference().child("Chats")
